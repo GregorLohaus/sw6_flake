faffcfd982f39513789c90be79594052d38dc7b3
Gregor Lohaus <gregor.lohaus@8mylez.com>
2023-11-05 17:34:32 +0100

    clearflake

---
+++ clearflake.sh
@@ -0,0 +1,28 @@
+chmod -R 755 .
+rm -r bin
+rm composer.json
+rm composer.lock
+rm -r config
+rm -r custom
+rm deprecation.ignore
+rm docker-compose.yaml
+rm -r mariadb
+rm my.cnf
+rm nginx.conf
+rm -r nginxlogs
+rm php-fpm.conf
+rm -r phpfpmlogs
+rm php-fpm.sock
+rm -r public
+rm redis.conf
+rm redis.log
+rm redis.pid
+rm -r services
+rm -r src
+rm -r tmp
+rm -r var
+rm -r vendor
+rm -r .cache
+rm -r .config
+rm -r .local
+rm phpstan.neon.dist
